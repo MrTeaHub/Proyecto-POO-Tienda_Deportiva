c3f562970fcf8233068b100cc5e99b84ff0c358b
Chivataj <chivataj@uninorte.edu.co>
2023-02-11 22:26:24 -0500

    Cambio atributo

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     Camiseta c("Camiseta de Colombia", "AFAXP22", "Amarillo","Adidas","Unisex","2023",
                "Algodon",120000,3,30,9.5,
-               "Futbol","Colombia", true,"V");
+               "Futbol","Colombia", false,"V");
     cout << c << endl;
     return 0;
 
