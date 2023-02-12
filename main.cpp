998dbeb136e5e7dac905eae502203915ebe36f06
Chivataj <chivataj@uninorte.edu.co>
2023-02-11 21:07:09 -0500

    Agrego la clase Producto y sus derivados

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 using namespace std;
 
 int main() {
-
+    Licra c("Rival 3", "XAALK312", "Negro", "SteelSeries",
+            "Unisex", "2022", "Pasta", 120000,
+            1, 6, 45.3, true, true);
+    cout << c;
     return 0;
 
 
