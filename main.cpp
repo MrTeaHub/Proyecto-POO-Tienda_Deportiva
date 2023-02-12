ab084ec82e6b1896a645b8bad79fc227b4446c1c
Chivataj <chivataj@uninorte.edu.co>
2023-02-11 21:46:39 -0500

    None

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include "clases/ropa/Licra.h"
+#include "clases/ropa/Camiseta.h"
 
 using namespace std;
 
 int main() {
+    Camiseta c("Camiseta de Colombia", "AFAXP22", "Amarillo","Adidas","Unisex","2023",
+               "Algodon",120000,3,30,9.5,
+               "Futbol","Colombia", true,"V");
+    cout << c << endl;
     return 0;
 
 
